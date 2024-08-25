ddd796772780f50bf78de21f31dfc96832072cbc
Oreste NGABO <orestengabo3@gmail.com>
2024-08-25 22:31:49 +0200

    Creating a Signup form for both mobile and web

---
+++ src/components/SignUp/SignUp.tsx
@@ -0,0 +1,80 @@
+import React, { useState } from "react";
+import { FaRegUser } from "react-icons/fa6";
+import { FaRegEnvelope } from "react-icons/fa";
+import { MdLockOutline } from "react-icons/md";
+import { ImCheckboxUnchecked, ImCheckboxChecked, ImFontSize } from "react-icons/im";
+import styles from "./SignUp.module.css";
+
+const SignupForm: React.FC = () => {
+  const [isChecked, setChecked] = useState(false)
+
+  const handleCheckBoxClick = () => {
+    setChecked((prevCheck) => !prevCheck)
+  }
+
+  return (
+    <div className={styles.signupForm}>
+      <p className="text-success fs-2 fw-bold text-right">Sign Up</p>
+      <p className="text-black fs-5">Welcome to the app</p>
+      <form>
+        <div className={styles.inputGroup}>
+          <FaRegUser className={styles.icon} size={20} />
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Username"
+            name="username"
+            required
+          />
+        </div>
+
+        <div className={styles.inputGroup}>
+          <FaRegEnvelope className={styles.icon} size={20} />
+          <input
+            type="email"
+            className="form-control"
+            placeholder="Email"
+            name="email"
+            required
+          />
+        </div>
+
+        <div className={styles.inputGroup}>
+          <MdLockOutline className={styles.icon} size={20} />
+          <input
+            type="password"
+            className="form-control"
+            placeholder="Password"
+            name="password"
+            required
+          />
+        </div>
+
+        <div className={styles.inputGroup}>
+          <MdLockOutline className={styles.icon} size={20} />
+          <input
+            type="password"
+            className="form-control"
+            placeholder="Confirm Password"
+            name="confirmPassword"
+            required
+          />
+        </div>
+
+        <div className={`${styles.inputGroup}`}>
+          <span onClick={handleCheckBoxClick} className={`${styles.icon}`}>
+            {!isChecked ? (<ImCheckboxUnchecked className={`${styles.icon}`} size={20} color="green" />) :
+            (<ImCheckboxChecked className={`${styles.icon} `} size={20} color="green" />)}
+          </span>
+          <p className="mx-5">I understood the terms and conditions</p>
+        </div>
+
+        <button type="submit" className={`${styles.submitButton} fw-semibold fs-4`}>
+          Sign Up
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default SignupForm;
